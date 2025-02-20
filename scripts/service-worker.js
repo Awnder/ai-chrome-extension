269ff0efd85699788f0ed28c4ff9c2c66ec27769
Andrew Shiroma <amshiroma@zoho.com>
2025-02-20 10:56:57 -0800

    added origin trial tokens

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,4 +1,11 @@
-function addClickToDocument() {
+function handleDocumentOnLoad() {
+    meta_tags = document.getElementsByTagName('meta');
+    for (let i = 0; i < meta_tags.length; i++) {
+        console.log(meta_tags[i]);
+    }
+    title = document.getElementsByTagName('title');
+    console.log(title[0]);
+
     document.addEventListener('click', function(event) {
         if (event) {
             console.log(event.target);
@@ -9,6 +16,7 @@ function addClickToDocument() {
 chrome.webNavigation.onCompleted.addListener(function(details) {
     chrome.scripting.executeScript({
         target: { tabId: details.tabId },
-        func: addClickToDocument
+        func: handleDocumentOnLoad
     })
 });
+
